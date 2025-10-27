1318378570307e624cfaf56ea4ceacd3dfd40e91
Uttam <kumaruttam519@gmail.com>
2025-10-27 18:34:40 +0530

    chore: improved the app and index file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
 import identifyRouter from "./routes/identify";
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
+app.use(express.json());
+
+// Routes
 app.use(identifyRouter);
-
-app.get("/", (req, res) => res.json({ status: "ok" }));
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-import dotenv from "dotenv";
 import app from "./app";
-import prisma from "./db";
 
-dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-async function main() {
-  app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server running on port ${port}`);
-  });
-}
-
-main().catch((e) => {
-  // eslint-disable-next-line no-console
-  console.error(e);
-  prisma.$disconnect();
-  process.exit(1);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
